9263ca0b377469b9fa8854a60e7451f8883a0561
Raj Sahu <167527803+rajsahu15@users.noreply.github.com>
2026-03-11 16:45:42 +0900

    LINQ CONPLEX DATA RETRIEVE

---
+++ LINQ_Complex_RetrieveData/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Linq;
+
+string serverName = "localhost\\SQLEXPRESS";
+string databaseName = "PracticeModule";
+
+string connectionString = "Server=" + serverName + ";Database=" + databaseName + ";Trusted_Connection=True;TrustServerCertificate=True;";
+
+using (SqlConnection con = new SqlConnection(connectionString))
+{
+    try
+    {
+        con.Open();
+        Console.WriteLine("Connection Established Successfully to " + databaseName);
+
+        string sqlQuery = "SELECT * FROM EMP";
+        SqlDataAdapter adapter = new SqlDataAdapter(sqlQuery, con);
+        DataTable empTable = new DataTable();
+        adapter.Fill(empTable);
+
+        var employees = from emp in empTable.AsEnumerable()
+                        where emp.Field<decimal?>("SALARY") > 10000
+                        select new
+                        {
+                            ID = emp.Field<int>("EMPLOYEE_ID"),
+                            FullName = emp.Field<string>("FIRST_NAME") + " " + emp.Field<string>("LAST_NAME"),
+                            Email = emp.Field<string>("EMAIL"),
+                            HireDate = emp.Field<DateTime>("HIRE_DATE"),
+                            Salary = emp.Field<decimal?>("SALARY")
+                        };
+
+
+
+        Console.WriteLine("\n--- High Earning Employees (LINQ Results) ---");
+        foreach (var e in employees)
+        {
+            Console.WriteLine("ID: " + e.ID +
+                              " | Name: " + e.FullName +
+                              " | Hired: " + e.HireDate.ToShortDateString() +
+                              " | Salary: $" + e.Salary);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+}
