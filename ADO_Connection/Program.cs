ca2f8a5aaf5d743290bc95ea120954c871ce822f
Raj Sahu <167527803+rajsahu15@users.noreply.github.com>
2026-03-11 16:39:57 +0900

    ADO Connection

---
+++ ADO_Connection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+// Using + for concatenation as requested
+string serverName = "localhost\\SQLEXPRESS";
+string databaseName = "PracticeModule";
+
+string connectionString = "Server=" + serverName + ";Database=" + databaseName + ";Trusted_Connection=True;TrustServerCertificate=True;";
+
+using (SqlConnection con = new SqlConnection(connectionString))
+{
+    try
+    {
+        con.Open();
+        Console.WriteLine("Connection Established Successfully to " + databaseName);
+    }
+    catch (Exception ex)
+    {
+        // Using + to combine the error message
+        Console.WriteLine("Error: " + ex.Message);
+    }
+}
